f5774b0f99921d3f3fd79c9c5f899cb2e5ca5d30
ice <102204002+aztice@users.noreply.github.com>
2025-07-19 20:44:33 +0800

    feat: 模型设备转移

--- server/apis/to.py
+++ server/apis/to.py
@@ -16,5 +16,13 @@ def to_dtype(name: str, dtype: str):
     elif response == False:
         return {"status": "error", "msg": "转换过程中出现问题"}
     return {"status": "success", "msg": "ok"}
+@router.get("/to/device/{name}/{device}")
+def to_device(name: str, device: str):
+    if not storage.model_exists(name):
+        return {"status": "error", "msg": "模型不存在"}
+    response = storage.to_device(name, device)
+    if response:
+        return {"status": "success", "msg": "ok"}
+    return {"status": "error", "msg": f"设备 {device} 不受支持。"}
 def register_routes(app):
     app.include_router(router)
