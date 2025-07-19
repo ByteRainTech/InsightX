088d46068c2cc53c362c7df5190db1d62affe863
ice <102204002+aztice@users.noreply.github.com>
2025-07-19 14:45:19 +0800

    fix: 修复模型挂载异常

--- server/apis/mount.py
+++ server/apis/mount.py
@@ -1,12 +1,25 @@
+"""
+这个项目作者是PHP全栈，不是Python开发，如果项目风格和你的不同，见谅，能用就行。
+"""
 from fastapi import APIRouter, Depends
 import os
 from typing import Optional
 from utils.model.storage import share_model,model_exists
 import torch
+import importlib.util
+import json
+import sys
 router = APIRouter()
 
+"""
+* name -> 模型名
+[+] path -> 模型目录
+[+] model_path -> 模型文件目录(.py)
+[+] model_class -> 模型调用的类
+[+] 模型调用初始化的参数
+"""
 @router.get("/mount/{source}")
-def mount(source: str, name: str, path: str = None, model_path: str = None, model_class: str = None, args: dict = {}):
+def mount(source: str, name: str, path: str = None, model_path: str = None, model_class: str = None, args: str = "{}"):
     match source: # 挂载方法
         case "torch": # TorchLoad
             if model_exists(name):
@@ -19,8 +32,14 @@ def mount(source: str, name: str, path: str = None, model_path: str = None, mode
                 return {"status": "error", "msg": "目标文件不存在，请设置参数 path。"}
             if not os.path.exists(path):
                 return {"status": "error", "msg": "模型文件不存在。"}
-            model = globals()[model_class](**args)
-            model.load_state_dict(torch.load(model_path))
+            preloaded = torch.load(path)
+            args = json.loads(args)
+            spec = importlib.util.spec_from_file_location("module_name", model_path)
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+            model_class = getattr(module, model_class)
+            model = model_class(**args)
+            model.load_state_dict(preloaded)
             share_model(name=name,model=model)
             return {"status": "success", "msg": "ok!"}
     return {"message": f""}
